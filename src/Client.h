5df614a4e41a225a380f0dbb9fa15c4835499e05
Josh Larson <joshlarson2015@gmail.com>
2020-01-08 12:59:07 -0500

    Fixed the issue with my head

--- src/Client.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-class Client {
-	public:
-	Client();
-	~Client();
-};
--- src/strfuncts.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <string>
-
-void clrNewlines(std::string &str);
-bool split(std::string &orig, std::string &left, std::string &right, const char delimiter);
-void lower(std::string &str);
-int hideInput(int fd, bool hide);
